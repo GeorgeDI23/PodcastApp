9f36ec357ef8ed8cd646c381dec016e88375f3ce
George Ionas <georgeion@Georges-MacBook-Pro.local>
2020-09-07 15:15:36 -0400

    initial SpringBoot framework

---
+++ src/main/java/ml/georgedi23/ion_casts/IonCastsApplication.java
@@ -0,0 +1,13 @@
+package ml.georgedi23.ion_casts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IonCastsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IonCastsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ml/georgedi23/ion_casts/IonCastsApplicationTests.java
@@ -0,0 +1,13 @@
+package ml.georgedi23.ion_casts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IonCastsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
