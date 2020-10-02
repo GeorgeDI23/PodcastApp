f6c3b66207d10f7d8f2785904d62c03c6b72bbd8
George Ionas <georgeion@Georges-MacBook-Pro.local>
2020-10-02 00:56:13 -0400

    implementing parse new podcast

--- src/main/java/ml/georgedi23/ion_casts/services/ParsingService.java
+++ src/main/java/ml/georgedi23/ion_casts/services/ParsingService.java
@@ -1,14 +1,22 @@
 package ml.georgedi23.ion_casts.services;
 
 import ml.georgedi23.ion_casts.models.Podcast;
-import ml.georgedi23.ion_casts.models.PodcastEpisode;
 
-import java.util.List;
+import javax.xml.stream.XMLEventReader;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.events.Characters;
+import javax.xml.stream.events.XMLEvent;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class ParsingService {
 
-    private Logger logger;
+    private final Logger logger;
 
     public ParsingService() {
         this.logger = Logger.getLogger(ParsingService.class.getName());
@@ -17,12 +25,65 @@ public class ParsingService {
     // Gets data for a new podcast
     public Podcast parseNewPodcast(String podcastUrl){
         Podcast podcast = new Podcast();
+        if(convertToURL(podcastUrl) != null){
+            try {
+                XMLEventReader eventReader = createXMLReader(convertToURL(podcastUrl));
+                podcast = getPodcastDetails(eventReader);
+                podcast.setLink(podcastUrl);
+            } catch(XMLStreamException | IOException e){
+                logger.log(Level.WARNING,"Error parsing XML; Exception: " + e.toString());
+                return null;
+            }
+        }
         return podcast;
     }
 
-    // returns all episodes of the podcast
-    public List<PodcastEpisode> getEpisodes(Podcast podcast){
-        return null;
+    public URL convertToURL(String podcastUrl){
+        try {
+            return new URL(podcastUrl);
+        } catch (MalformedURLException e) {
+            logger.log(Level.WARNING,"Unable to parse URL; Exception: " + e.toString());
+            return null;
+        }
+    }
+
+    public XMLEventReader createXMLReader(URL podcastUrl) throws XMLStreamException, IOException {
+        XMLInputFactory inputFactory = XMLInputFactory.newInstance();
+        InputStream inputStream = podcastUrl.openStream();
+        return inputFactory.createXMLEventReader(inputStream);
+    }
+
+    public Podcast getPodcastDetails(XMLEventReader eventReader) throws XMLStreamException {
+        Podcast podcast = new Podcast();
+        xmlLoop: while(eventReader.hasNext()) {
+            XMLEvent event = eventReader.nextEvent();
+            if(event.isStartElement()){
+                String localPart = event.asStartElement().getName().getLocalPart();
+                switch (localPart) {
+                    case "title":
+                        podcast.setTitle(getCharacterData(eventReader));
+                        break;
+                    case "url":
+                        podcast.setImage(getCharacterData(eventReader));
+                        break;
+                    case "description":
+                        podcast.setDescription(getCharacterData(eventReader));
+                        break;
+                    case "item":
+                        break xmlLoop;
+                }
+            }
+        }
+        return podcast;
+    }
+
+    public String getCharacterData(XMLEventReader eventReader) throws XMLStreamException {
+        String result = "";
+        XMLEvent event = eventReader.nextEvent();
+        if (event instanceof Characters) {
+            result = event.asCharacters().getData();
+        }
+        return result;
     }
 }
 
--- src/test/java/ml/georgedi23/ion_casts/services/ParsingServiceTest.java
+++ src/test/java/ml/georgedi23/ion_casts/services/ParsingServiceTest.java
@@ -1,9 +1,133 @@
 package ml.georgedi23.ion_casts.services;
 
-import static org.junit.Assert.*;
+import ml.georgedi23.ion_casts.models.Podcast;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 
+import javax.xml.stream.XMLEventReader;
+import javax.xml.stream.XMLStreamException;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+import static org.junit.Assert.*;
+
+@RunWith(MockitoJUnitRunner.class)
 public class ParsingServiceTest {
 
+    ParsingService parsingService;
+    @Mock
+    private URL testUrl = Mockito.mock(URL.class);
+
+    @Before
+    public void setUp(){
+        parsingService = new ParsingService();
+    }
+
+    @Test
+    public void convertToUrlTest() {
+        //Given
+        String givenUrl = "https://www.google.com";
+
+        //When
+        URL actual =  parsingService.convertToURL(givenUrl);
+
+        //Then
+        assertEquals(givenUrl, actual.toString());
+    }
+
+    @Test
+    public void convertToUrlExceptionTest() {
+        //Given
+        String givenUrl = "www.google.com";
+
+        //When
+        URL actual =  parsingService.convertToURL(givenUrl);
+
+        //Then
+        assertNull(actual);
+    }
+
+    @Test
+    public void createXMLReaderTest() throws IOException, XMLStreamException {
+        // Given
+        String expected = "<title>";
+        Mockito.when(testUrl.openStream()).thenReturn(stubXML());
+
+        // When
+        XMLEventReader reader = parsingService.createXMLReader(testUrl);
+        reader.nextEvent();
+        reader.nextEvent();
+        reader.nextEvent();
+        reader.nextEvent();
+        String actual = reader.nextTag().toString();
+
+        // Then
+        assertEquals(expected, actual);
+
+    }
+
+    public InputStream stubXML(){
+        String initialString = "<rss xmlns:media=\"http://search.yahoo.com/mrss/\" xmlns:content=\"http://purl.org/rss/1.0/modules/content/\" version=\"2.0\">\n" +
+                "<channel>\n" +
+                "<title>Podcast Title</title>\n" +
+                "<description>\n" +
+                "Podcast description was here." +
+                "</description>\n" +
+                "<image>\n" +
+                "<url>\n" +
+                "https://images.test.fm/test.jpg" +
+                "</url>\n" +
+                "</image>\n" +
+                "<item>\n" +
+                "<title>75: Title is Here</title>\n" +
+                "<link>https://test.com/episode/75</link>\n" +
+                "<description>\n" +
+                "Episode description was here." +
+                "</description>\n" +
+                "<pubDate>Tue, 29 Sep 2020 07:00:00 -0000</pubDate>\n" +
+                "<guid isPermaLink=\"false\">\n" +
+                "</guid>\n" +
+                "<enclosure url=\"https://www.podtrac.com/something.mp3\" length=\"0\" type=\"audio/mpeg\"/>\n" +
+                "</item>";
+        return new ByteArrayInputStream(initialString.getBytes());
+    }
+
+    @Test
+    public void getCharacterDataTest() throws IOException, XMLStreamException {
+        // Given
+        String expected = "Podcast Title";
+        Mockito.when(testUrl.openStream()).thenReturn(stubXML());
+
+        // When
+        XMLEventReader reader = parsingService.createXMLReader(testUrl);
+        for(int i = 0; i < 6; i++) reader.nextEvent();
+        String actual = parsingService.getCharacterData(reader);
+
+        //Then
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getPodcastDetailsTest() throws IOException, XMLStreamException {
+        // Given
+        String expected = "Podcast Title\n\nPodcast description was here.\n\nhttps://images.test.fm/test.jpg";
+        Mockito.when(testUrl.openStream()).thenReturn(stubXML());
+
+        // When
+        XMLEventReader reader = parsingService.createXMLReader(testUrl);
+        Podcast actualPodcast = parsingService.getPodcastDetails(reader);
+        String actual = actualPodcast.getTitle();
+        actual = actual.concat("\n" + actualPodcast.getDescription() + "\n");
+        actual = actual.concat(actualPodcast.getImage());
+
+        //Then
+        assertEquals(expected, actual);
+    }
 
 }
